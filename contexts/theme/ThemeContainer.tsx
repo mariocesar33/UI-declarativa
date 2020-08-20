50765baabcc90adb6dea52c410f569517df4e338
Mário César Silva <mcgato33@hotmail.com>
2020-08-20 13:36:31 -0100

    inicio

--- contexts/theme/ThemeContainer.tsx
+++ contexts/theme/ThemeContainer.tsx
@@ -1,1 +1,9 @@
 import React from 'react';
+
+import { ThemeProvider as ChakraThemeProvider } from '@chakra-ui/core';
+
+const ThemeContainer: React.FC = ({ children}) => {
+  return (
+    <ChakraThemeProvider></ChakraThemeProvider>
+  );
+};
